3b4347b921a9fc27c86db647caeb013a22b6455b
Ishani Mariathasan <ishani.mariathasan@gmail.com>
2025-09-06 18:14:48 +0100

    initial

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.write('Hi')
+st.title("Project health data")
+st.header("BMI Calculator")
+st.subheader("Because that hasn't been done before...")
+if st.button("Calculate BMI Now"):
+
+    gender = st.radio("Select gender:", ['Female','Male'])
+    if gender == 'Male':
+        st.succcess("Male confirmed")
+    elif gender == "Female":
+        st.success("Female confirmed")
+    else:
+        st.error("")
+
+else:
+    options = st.selectbox("Select an option", ['About', 'Data analysis', 'Plans'])
+
+
